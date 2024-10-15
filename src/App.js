442deb659f29f15c4505e84e1ec8fea0b0c0b812
BehJiaLing <p24016535@student.newinti.edu.my>
2024-10-15 17:33:48 +0800

    upd: Updated the default running page is login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./login"; 
 import Dashboard from "./dashboard";  
 import ErrorPage from "./error";  
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/error" element={<ErrorPage />} />
