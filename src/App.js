e60dc788f641da7326de3dcf2fd5a71bdbbca8a8
BehJiaLing <p24016535@student.newinti.edu.my>
2024-10-15 13:50:14 +0800

    upd: Updated the App file to display the login page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import Login from "./login";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <div>
-          <label for="fname">First name:</label>
-        </div>
-      </header> */}
       <Login />
     </div>
   );
