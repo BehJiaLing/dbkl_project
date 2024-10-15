56717868a0f7c18b5dee0e15f7cad6a66ec507e4
BehJiaLing <p24016535@student.newinti.edu.my>
2024-10-15 16:32:11 +0800

    upd: Added an error route path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./login"; 
 import Dashboard from "./dashboard";  
+import ErrorPage from "./error";  
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/error" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
