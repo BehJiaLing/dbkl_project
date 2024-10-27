3cd7d1c4d427b280c5023352096ea4cc438440cc
BehJiaLing <p24016535@student.newinti.edu.my>
2024-10-27 11:33:33 +0800

    upd: close the console of page data from db

--- models/pageModel.js
+++ models/pageModel.js
@@ -3,7 +3,7 @@ const db = require('../config/db');
 const PageModel = {
     getPageData: async () => {
         const [rows] = await db.query('SELECT admin_id, page_id FROM adminpageaccess');
-        console.log('Page data:', rows); 
+        // console.log('Page data:', rows); 
         return rows; 
     },
 };
